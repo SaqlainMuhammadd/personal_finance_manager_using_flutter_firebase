5da2702ec5a0b9296447a5b9f3de335cf7f36e99
saqlainmuhammadd <miansaqlainqureshi@gmail.com>
2024-07-26 17:34:14 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/personal_finance_manager_using_flutter_firebase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.personal_finance_manager_using_flutter_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:personal_finance_manager_using_flutter_firebase/auth/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Finance Manager',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
